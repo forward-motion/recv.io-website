93e60b658f24789e14951e32246c7412bc7c2d0f
Eric Sanchez <eric.sanchez@domandtom.com>
2018-04-04 23:14:21 -0400

    adding checkboxes

--- src/components/compare/ComparePage.jsx
+++ src/components/compare/ComparePage.jsx
@@ -100,6 +100,33 @@ class ComparePage extends Component {
           </div>
         </div>
 
+        <div className="row">
+          <div className="col-md-12">
+            <h2>Features:</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <input type="checkbox" /> Choice 1
+          </div>
+          <div className="col-md-6">
+            <input type="checkbox" /> Choice 2
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <input type="checkbox" /> Choice 3
+          </div>
+          <div className="col-md-6">
+            <input type="checkbox" /> Choice 4
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <h2>Results:</h2>
+          </div>
+        </div>
       </div>
     );
   }
