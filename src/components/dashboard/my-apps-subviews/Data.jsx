cd444cdebbf8b7b0f2feb52ac3ce65a10a167bfd
Caesar Bell <caesar.jd.bell@gmail.com>
2018-04-08 15:03:45 -0400

    table is now implemented, and so is the custom buttons to generate the different data for the tables

--- src/components/dashboard/my-apps-subviews/Data.jsx
+++ src/components/dashboard/my-apps-subviews/Data.jsx
@@ -1,11 +1,150 @@
 import React from 'react';
+import ReactTable from 'react-table'
 
-const Data = () => (
-    <div>
+import '../../../styles/dashboard/my-apps-subviews/Data.scss';
 
-        <h1>Data</h1>
+import { messages , clients , users } from '../../../data/data.js'; 
 
-    </div>
-);
+
+class Data extends React.Component {
+    constructor(props) {
+        super(props); 
+        
+
+        this.state = {
+            selectorButtons: ['message', 'clients', 'users'],
+            selectedTable: 'message', 
+            messages: messages,
+            clients: clients,
+            users: users
+        }
+
+        this.onChangeTable = this.onChangeTable.bind(this);
+
+
+    }
+
+    onChangeTable(e) {
+        
+        this.setState({ selectedTable : e.target.name }); 
+
+    }
+
+    get tableSelectorButtons() {
+
+        return this.state.selectorButtons.map((button) => {
+            return (
+                <div>
+                    <button
+                        className="btn table-btn-selector"
+                        onClick={this.onChangeTable}
+                        name={button}
+                    >
+                        {button}
+                    </button>
+                    <div className={`under-line ${this.state.selectedTable == button ? 'active' : null }`}></div>
+                </div>
+            );
+        })
+    }
+
+    get showSelctedTable() {
+
+        let data = null; 
+        let columns = null;
+
+        switch (this.state.selectedTable) {
+            case 'message':
+                data = this.state.messages; 
+                columns = [
+                    {
+                        Header: 'Topic',
+                        accessor: 'topic'
+                    },
+                    {
+                        Header: 'Payload',
+                        accessor: 'payload'
+                    },
+                    {
+                        Header: 'Date',
+                        accessor: 'date'
+                    }
+                ]; 
+                
+                break;
+            case 'clients': 
+                data = this.state.clients;
+                columns = [
+                    {
+                        Header: 'ID',
+                        accessor: 'id'
+                    },
+                    {
+                        Header: 'User ID',
+                        accessor: 'user_id'
+                    },
+                    {
+                        Header: 'Date Created',
+                        accessor: 'date_created'
+                    },
+                    {
+                        Header: 'Status',
+                        accessor: 'status'
+                    }
+                ];
+
+                break;
+            case 'users': 
+                data = this.state.users;
+                columns = [
+                    {
+                        Header: 'ID',
+                        accessor: 'id'
+                    },
+                    {
+                        Header: 'Clients',
+                        accessor: 'clients'
+                    },
+                    {
+                        Header: 'Status',
+                        accessor: 'status'
+                    }
+                ];
+
+                break;
+        }
+
+        return (
+            <ReactTable
+                defaultPageSize={5}
+                data={data}
+                columns={columns}
+            />
+        );
+
+
+    }
+
+
+
+    render() {
+
+
+        return (
+            <div className="container-fluid data-page">
+        
+                <div className="row">
+                    <div className="col-md-2">
+                        { this.tableSelectorButtons }
+                    </div>
+                    <div className="col-md-10">
+                        { this.showSelctedTable }
+                    </div>
+                </div>
+        
+            </div>
+        );
+    }
+}
 
 export default Data;
