d21a1cbd52c7665f6a461b885597d4ab84302240
Eric Sanchez <eric.sanchez@domandtom.com>
2018-04-14 18:06:19 -0400

    added styled to code snip

--- src/components/home/sections/PubSub.jsx
+++ src/components/home/sections/PubSub.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import '../../../styles/home/sections/PubSub.scss';
+import Link from 'gatsby-link';
 
-const PubSub = () => (
-  // <div className="component-sign-up">
-  <div className="component-pub-sub">
-    <div className="code-sample row">
-        <div className="subscribe col-md-6">
-            <h2>Subscribe</h2>
-            <pre>{`
-          <div>
-            hello world
-          <div>
-            `}
-        </pre>
-        </div>
-        <div className="publish col-md-6">
-            <h2>Publish</h2>
-            <pre>{`
-          <div>
-            hello friend
-          <div>
-            `}
-        </pre>
-        </div>
+import '../../../styles/home/sections/PubSub.scss';
+import BackgroundImage from '../../../img/images/home/pub-sub-bg.png';
+import CodeSnip1 from '../../../img/images/home/codesnip-1.png';
+import CodeSnip2 from '../../../img/images/home/codesnip-2.png';
+
+function renderCodeSnip(title, image, link) {
+  return (
+    <div className="code-snip code-snip-1 col-md-6">
+      <div className="title-wrapper">
+        <Link to={link}>
+          <div className="title">{title}</div>
+        </Link>
+        <div className="title-underline"></div>
+      </div>
+      <div className="image"
+        style={{
+        backgroundImage:`url(${image})`,
+        backgroundRepeat:"no-repeat",
+        backgroundPosition:"center",
+        backgroundSize:"98%",
+      }}></div>
     </div>
+  );
+}
+const PubSub = () => (
+  <div className="component-pub-sub"
+    style={{
+      backgroundImage:`url(${BackgroundImage})`,
+      backgroundRepeat:"no-repeat",
+      backgroundPosition:"center"
+    }}>
+    <div className="codesnip-wrapper row">
+      {renderCodeSnip('subscribe',CodeSnip1, '#' )}
+      {renderCodeSnip('publish',CodeSnip2, '#' )}
+    </div>
+
   </div>
 );
 
