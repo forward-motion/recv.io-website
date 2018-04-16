2251f6fa200d64825cb973d388c7db751f0de435
Caesar Bell <caesar.jd.bell@gmail.com>
2018-04-15 20:54:22 -0400

    syles implemented

---
+++ src/components/_global/ui/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'; 
+import firebase from 'firebase';
+import FirebaseAuth from 'react-firebaseui/FirebaseAuth';
+import moment from 'moment';
+
+
+import '../../../styles/_global/ui/Footer.scss';
+
+const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        signInSuccess: () => false
+    }
+};
+
+
+const Footer = () => {
+    return(
+        <div className="page-footer">
+            <div className="col-xs-12">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p className="call-to-action-sign-up">Ready to recv<span>?</span> Sign up for <b>free</b> with</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <p className="copy-right">© {moment().year()} - recv.io All Right Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/pricing/PricingPage.jsx
+++ src/components/pricing/PricingPage.jsx
@@ -1,5 +1,7 @@
 import React , { Component } from 'react';
+import { PanelGroup, Panel, Heading, Title, Body } from 'react-bootstrap';
 import Link from 'gatsby-link';
+import Footer from '../_global/ui/Footer.jsx';
 
 import '../../styles/pricing/PricingPage.scss';
 
@@ -8,44 +10,118 @@ class PricingPage extends Component {
         super(props);
 
         this.state = {
+            activeKey: '',
             prices: [
                 {
+                    icon: 'icon-free',
                     plan: 'free',
                     includes: ['lmiited monitoring', '4 apps', '10k streams', '-']
                 },
                 {
+                    icon: 'icon-paid',
                     plan: 'paid',
                     includes: ['full monitoring', 'unlimited apps', '100k streans', 'realtime connections']
                 },
                 {
-                    plan: 'standalone network',
+                    icon: 'icon-standalone',
+                    plan: 'standalone',
                     includes: ['access to Jarves', 'robot build apps for you', 'unlimited streams', 'end-to-end connections']
+                }
+            ],
+            faqs: [
+                {
+                    key: '1',
+                    heading: 'Lorem ipsum dolor sit amet, his odio omnium ad, mea minimum consequat democritum an. Ut cum feugiat vocibus forensibus, eros suscipiantur ?',
+                    questions: [
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.'                        
+                    ]
+                },
+                {
+                    key: '2',
+                    heading: 'Lorem ipsum dolor sit amet, his odio omnium ad, mea minimum consequat democritum an. Ut cum feugiat vocibus forensibus, eros suscipiantur ?',
+                    questions: [
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.'
+                    ]
+                },
+                {
+                    key: '3',
+                    heading: 'Lorem ipsum dolor sit amet, his odio omnium ad, mea minimum consequat democritum an. Ut cum feugiat vocibus forensibus, eros suscipiantur ?',
+                    questions: [
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.'
+                    ]
+                },
+                {
+                    key: '4',
+                    heading: 'Lorem ipsum dolor sit amet, his odio omnium ad, mea minimum consequat democritum an. Ut cum feugiat vocibus forensibus, eros suscipiantur ?',
+                    questions: [
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.',
+                        'bus elaboraret cu, illud fugit conceptam vis in, eam mundi tation bonorum cu. Illum incorrupte contentiones eu pri.'
+                    ]
                 }
             ]
         }; 
 
+        this.onHandleSelect = this.onHandleSelect.bind(this);
+
+    }
+
+
+    onHandleSelect(activeKey) {
+
+        this.setState({ activeKey });
     }
 
     get plans() {
 
         return this.state.prices.map((price) => {
             return(
-                <div className="col-md-4">
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            { price.plan }
-                        </div>
-                        <div className="panel-body">
+                <div className="panel panel-default" style={{width : '300px'}}>
+                    <div className="panel-heading">
+                        <p style={{ fontSize : '35px'}}><span className={ price.icon }></span></p>
+                        <p className="price-plan">{ price.plan }</p>
+                    </div>
+                    <div className="panel-body">
+                        <ul>
                             {(() => {
                                 return price.includes.map((include) => {
-                                    return(
-                                        <p> {include }</p>
+                                    return (
+                                        <li> { include }</li>
                                     );
                                 })
                             })()}
-                        </div>
+                        </ul>
                     </div>
                 </div>
+            );
+        })
+    }
+
+    get accordionPanels() {
+        return this.state.faqs.map((faq) => {
+            return(
+                <Panel key={faq.key} eventKey={faq.key} style={{ border : 'none'}}>
+                    <Panel.Heading>
+                        <Panel.Title toggle> <span className="toggle-icon"> { this.state.activeKey === faq.key ? '-' : '+' }</span> { faq.heading }</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body collapsible style={{paddingLeft : '30px', borderTop : 'none'}}>
+                        {(() => {
+                            return faq.questions.map((question) => {
+                                return(
+                                    <div>
+                                        <p> { question }</p>
+                                    </div>
+                                );
+                            })
+                        })()}
+                    </Panel.Body>
+                </Panel>
             );
         })
     }
@@ -53,25 +129,81 @@ class PricingPage extends Component {
     render() {
 
         return(
-            <div className=" container-fluid pricing-page">
+            <div className="pricing-page">
 
-                <h1>Pricing Page</h1>
+                <div className="container-fluid">
 
-                <div>
-                    <h4>Our pricing scales to</h4>
+                    <div className="row">
 
-                    { this.plans }
+                        <div className="col-md-12">
 
-                </div>
-                
-                <div>
-                    <div className="comparing">
-                        <p>Ses how we compare to the competition <span><Link to="/compare">Here</Link></span></p>
+                            <h2 className="tag-line">Our pricing scales too.</h2>
+
+                        </div>
+
                     </div>
-                </div>
 
-                <div className="fqa">
-                    <h4>FAQ</h4>
+                    <div className="row">
+
+                        <div style={{margin : '0 auto', width: '100px'}}>
+                            <div style={{marginBottom : '100px'}} className="underline-accent"></div>
+                        </div>
+
+                    </div>
+
+                    <div className="row" style={{backgroundColor : 'white', marginTop : '40px'}}>
+
+                       <div style={{
+                            margin : '0 auto', 
+                            marginTop : '-50px',
+                            marginBottom : '40px',
+                            width: '980px'
+                           }}>
+                            {this.plans}
+                       </div>
+
+                        <div style={{ margin: '0 auto', width: '100px' }}>
+                            <div style={{ marginBottom : '50px'}} className="underline-accent"></div>
+                        </div>
+
+                    </div>
+
+                    <div className="row">
+
+                        <div className="comparing">
+                            <p>see how we compare to the competition <span><Link to="/compare">here</Link></span>.</p>
+                        </div>
+
+                    </div>
+
+                    <div className="row" style={{ backgroundColor : 'white'}}>
+
+                        <div className="col-md-12">
+                            <div className="faq">
+                                <p style={{fontSize : '35px'}}><span className="icon-faqs"></span></p>
+                                <h4>FAQ</h4>
+                            </div>
+                        </div>
+
+                        <div className="col-md-12">
+                           <PanelGroup
+                                accordion
+                                id="faq-accordion"
+                                activeKey={this.state.activeKey}
+                                onSelect={this.onHandleSelect}
+                           >
+
+                            { this.accordionPanels }
+
+                           </PanelGroup>
+                        </div>
+
+                    </div>
+
+                    <div className="row">
+                        <Footer />
+                    </div>
+
                 </div>
             </div>
         );
