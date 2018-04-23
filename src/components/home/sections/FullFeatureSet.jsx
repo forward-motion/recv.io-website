5774ce11a421c43b9a97df938d8ffc09de2e5383
Eric Sanchez <eric.sanchez@domandtom.com>
2018-04-22 22:00:58 -0400

    moved here link in same span as leading sentence

--- src/components/home/sections/FullFeatureSet.jsx
+++ src/components/home/sections/FullFeatureSet.jsx
@@ -4,7 +4,7 @@ import '../../../styles/home/sections/FullFeatureSet.scss';
 
 const FullFeatureSet= (props) => (
   <div className="component-full-feature-set">
-        <span><span className="feature-text">see our full feature set</span> <span className="here"><Link to="#">here</Link></span><span className="feature-text">.</span> </span>
+        <span className="feature-text">see our full feature set <Link to="#">here</Link>.</span>
   </div>
 );
 export default FullFeatureSet;
